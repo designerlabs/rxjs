7a7c26e05636841fac775dd32c4a947438cb09a6
Sridhar Swaminathan <designerlabs@hotmail.com>
2018-01-03 22:41:52 +0800

    updated

--- main.ts
+++ main.ts
@@ -1,7 +1,31 @@
-import { Observable } from "rxjs";
+import { Observable } from "rxjs"
+// import { Observable } from "rxjs/Observable";
+// import "rxjs/add/operator/map";
+// import "rxjs/add/operator/filter";
+
+
+let output = document.getElementById("output");
+let button = document.getElementById("button");
 
 let numbers = [1, 5, 10];
 // let source = Observable.from(numbers);
+
+
+let click = Observable.fromEvent(button,'click');
+
+let mouseH = Observable.fromEvent(document,'mousemove')
+                        .map((e:MouseEvent) => {
+                                return {
+                                    x: e.clientX,
+                                    y: e.clientY
+                                }
+                        })
+                        .filter(value => value.x < 500)
+                        .delay(300);
+
+                        
+
+
 let source = Observable.create(
     observer => {
 
@@ -26,11 +50,47 @@ let source = Observable.create(
 .map(n => n * 2)
 .filter(n => n > 4);
 
+
+
+// function onNext(value){
+//     circle.style.left = value.x;
+//     circle.style.top = value.y;
+// }
+
+function load(url: string){
+ let xhr = new XMLHttpRequest();
+ xhr.addEventListener("load", ()=>{
+     let movies = JSON.parse(xhr.responseText);
+     movies.forEach(element => {
+         let div = document.createElement('div');
+         div.innerText = element.title;
+         output.appendChild(div);
+     });
+ })
+ xhr.open("GET", url, true);
+ xhr.send();
+}
+
+click.subscribe(
+    e => load("movie.json"),
+    e => console.log(`error: ${e}`),
+    () => console.log("complete")
+);
+
+
+// mouseH.subscribe(
+//     value => console.log(value),
+//     e => console.log(`error: ${e}`),
+//     () => console.log("complete")
+// );
+
 source.subscribe(
     value => console.log(`value: ${value}`),
     e => console.log(`error: ${e}`),
     () => console.log("complete")
 );
+
+
 
 // class MyObserver{
 //     next(value){
