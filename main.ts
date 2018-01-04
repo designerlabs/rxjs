c255391f6f752a04b5d090c390bbe756869a7f09
Sridhar Swaminathan <sridhar.snathan@mimos.my>
2018-01-04 18:09:21 +0800

    update observer

--- main.ts
+++ main.ts
@@ -11,31 +11,31 @@ let numbers = [1, 5, 10];
 // let source = Observable.from(numbers);
 
 
-let click = Observable.fromEvent(button,'click');
+let click = Observable.fromEvent(button, 'click');
 
-let mouseH = Observable.fromEvent(document,'mousemove')
-                        .map((e:MouseEvent) => {
-                                return {
-                                    x: e.clientX,
-                                    y: e.clientY
-                                }
-                        })
-                        .filter(value => value.x < 500)
-                        .delay(300);
+let mouseH = Observable.fromEvent(document, 'mousemove')
+    .map((e: MouseEvent) => {
+        return {
+            x: e.clientX,
+            y: e.clientY
+        }
+    })
+    .filter(value => value.x < 500)
+    .delay(300);
 
-                        
+
 
 
 let source = Observable.create(
     observer => {
 
-        let index=0;
+        let index = 0;
         let produceValue = () => {
             observer.next(numbers[index++]);
 
-            if(index < numbers.length){
+            if (index < numbers.length) {
                 setTimeout(produceValue, 200);
-            }else{
+            } else {
                 observer.complete();
             }
         }
@@ -47,8 +47,8 @@ let source = Observable.create(
         // observer.complete();
     }
 )
-.map(n => n * 2)
-.filter(n => n > 4);
+    .map(n => n * 2)
+    .filter(n => n > 4);
 
 
 
@@ -57,25 +57,88 @@ let source = Observable.create(
 //     circle.style.top = value.y;
 // }
 
-function load(url: string){
- let xhr = new XMLHttpRequest();
- xhr.addEventListener("load", ()=>{
-     let movies = JSON.parse(xhr.responseText);
-     movies.forEach(element => {
-         let div = document.createElement('div');
-         div.innerText = element.title;
-         output.appendChild(div);
-     });
- })
- xhr.open("GET", url, true);
- xhr.send();
+
+/* Observable way */
+
+function loadObs(url: string) {
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("load", () => {
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+            }
+            else{
+                observer.error(xhr.statusText);
+            }
+
+
+        });
+        xhr.open("GET", url, true);
+        xhr.send();
+    }).retryWhen(retryStrategy({attempts:2, delay: 1000}))//retry(3)
 }
 
-click.subscribe(
-    e => load("movie.json"),
+function retryStrategy({attempts=4, delay = 4000}){
+    return function (errors){
+        return errors
+            .scan((acc, value) => {
+                console.log(acc, value);
+                return acc + 1;
+            },  0)    
+            .takeWhile(acc => acc < attempts)
+            .delay(delay);
+    }
+}
+
+function renderMovies(movies) {
+    movies.forEach(element => {
+        let div = document.createElement('div');
+        div.innerText = element.title;
+        output.appendChild(div);
+    });
+}
+
+
+
+
+function load(url: string) {
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", () => {
+        let movies = JSON.parse(xhr.responseText);
+        movies.forEach(element => {
+            let div = document.createElement('div');
+            div.innerText = element.title;
+            output.appendChild(div);
+        });
+    })
+    xhr.open("GET", url, true);
+    xhr.send();
+}
+
+
+/*FlatMap 
+*/
+click.flatMap(e => loadObs("movies.json"))
+    .subscribe(
+    renderMovies,
     e => console.log(`error: ${e}`),
     () => console.log("complete")
-);
+    );
+
+
+
+/* Map observable
+click.map(e => loadObs("movies.json"))
+    .subscribe(o => console.log(o));
+*/
+// click.subscribe(
+//     e => load("movie.json"),
+//     e => console.log(`error: ${e}`),
+//     () => console.log("complete")
+// );
 
 
 // mouseH.subscribe(
